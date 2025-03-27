acf55fa13573809e3db5c115c9a4695eb984d558
Holl4444 <holly.cello@gmail.com>
2025-03-27 11:54:34 +0000

    Add About text

---
+++ src/components/About.tsx
@@ -0,0 +1,35 @@
+export default function About() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>
+        Hey I'm Holly. I transitioned to software engineering from
+        music and education. I've always loved creating things, from
+        the physical, like pillow forts and makeshift tv antennas to
+        the more ephemeral, like music and great experiences for
+        pupils. Programming to me is the perfect balance, where I can
+        produce something equal parts practical and magical!
+      </p>
+      <p>
+        Having spent some time living in Sweden I was incredibly
+        impressed by the availability of quality education for all and
+        I'd love to use coding to contribute towards making knowledge
+        free world-wide.
+      </p>
+      <p>
+        I love a challenge and have found plenty both in tackling
+        back-end APIs and servers and front-end React states and
+        responsive css. I'm drawn to front-end's endless and playful
+        possibilities for visuals and interactions and love working
+        towards great user experiences. Equally I enjoy tinkering,
+        seeing how everything joins together and that dopamine hit
+        from cracking a logic conundrum!
+      </p>
+      <p>
+        I've recently graduated from an intensive coding bootcamp and
+        am looking for my first professional role in tech.
+          </p>
+        <p>If you'd like to chat, you can contact me through the form below, or reach out on linked in. Feel free to vi</p>
+    </>
+  );
+}
